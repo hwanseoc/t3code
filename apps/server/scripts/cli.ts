501bf3599efc5d82731edc58c6f19a763a2985c1
Hwanseo Choi <electricvoxel@gmail.com>
2026-03-31 02:09:46 +0900

    Use production build icons and cache static assets
    (cherry picked from commit 080066c3363aed9fc96400583a1e9f9ab913260b)
    (cherry picked from commit 91cfa6f35a2b6c3213ca54a67a078a769819b32f)

--- apps/server/scripts/cli.ts
+++ apps/server/scripts/cli.ts
@@ -11,10 +11,7 @@ import * as Schema from "effect/Schema";
 import { Command, Flag } from "effect/unstable/cli";
 import { ChildProcess, ChildProcessSpawner } from "effect/unstable/process";
 
-import {
-  DEVELOPMENT_ICON_OVERRIDES,
-  PUBLISH_ICON_OVERRIDES,
-} from "../../../scripts/lib/brand-assets.ts";
+import { PUBLISH_ICON_OVERRIDES } from "../../../scripts/lib/brand-assets.ts";
 import { resolveCatalogDependencies } from "../../../scripts/lib/resolve-catalog.ts";
 import { fromJsonStringPretty } from "@t3tools/shared/schemaJson";
 import rootPackageJson from "../../../package.json" with { type: "json" };
@@ -117,7 +114,7 @@ const applyDevelopmentIconOverrides = Effect.fn("applyDevelopmentIconOverrides")
   const path = yield* Path.Path;
   const fs = yield* FileSystem.FileSystem;
 
-  for (const override of DEVELOPMENT_ICON_OVERRIDES) {
+  for (const override of PUBLISH_ICON_OVERRIDES) {
     const sourcePath = path.join(repoRoot, override.sourceRelativePath);
     const targetPath = path.join(serverDir, override.targetRelativePath);
 
